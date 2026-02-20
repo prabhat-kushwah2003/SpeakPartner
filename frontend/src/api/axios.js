e5fd0a3b8e78aa83ad4efb2e09b2d9a466c925b7
prabhat-kushwah2003 <prabhatkushwah2003@email.com>
2026-02-20 16:04:40 +0530

    changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
   // baseURL: "https://zpr7gwss-5000.inc1.devtunnels.ms/",
 });
 
